baebbce05b6b7190c3909d08249fb0e069e15c69
Youssef Elmoudene <youssefelmoudene09@gmail.com>
2022-08-22 11:27:30 +0100

    Initial commit

---
+++ src/main/java/com/yena/yenaoptic/YenaOpticApplication.java
@@ -0,0 +1,13 @@
+package com.yena.yenaoptic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YenaOpticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YenaOpticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yena/yenaoptic/YenaOpticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yena.yenaoptic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YenaOpticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
